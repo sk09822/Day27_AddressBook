4ee9b977c126528dbaa3455290589ed734a31152
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-28 14:32:02 +0530

    Added : Address Book System UC1 TO UC12

---
+++ src/main/java/com/blz/day27/ContactDetails.java
@@ -0,0 +1,27 @@
+package com.blz.day27;
+
+public class ContactDetails {
+    String firstName, lastName, email;
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getEmail(){
+        return email;
+    }
+}
+
+
+
